f1be481f50e88c95f04d868231e90b3bc9d3ea02
COBBINA EMMANUEL ADU BOATENG <aduboateng333@gmail.com>
2023-09-14 14:59:54 +0000

    added the necessary data

---
+++ src/data.js
@@ -0,0 +1,38 @@
+import adu from "../src/Assets/Images/adu.jpeg"
+import david from "../src/Assets/Images/david.jpg"
+import luna from "../src/Assets/Images/luna.jpg"
+import rex from "../src/Assets/Images/rex.jpg"
+import vee from "../src/Assets/Images/vee.jpg"
+
+export  const data = (
+    {
+        id: 1,
+        name: "Rexford Brefo",
+        age: 21,
+        image: {rex}
+    },
+    {
+        id: 2,
+        name: "Valerie Ofori",
+        age: 21,
+        image: {vee}
+    },
+    {
+        id: 3,
+        name: "Adu Boat",
+        age: 20,
+        image: {adu}
+    },
+    {
+        id: 4,
+        name: "Luna",
+        age: 19,
+        image: {luna}
+    },
+    {
+        id: 5,
+        name: "David",
+        age: 18,
+        image: {david}
+    }
+)
