e0e4c4b721fe9431a017dc0c297920dbb08d30da
COBBINA EMMANUEL ADU BOATENG <aduboateng333@gmail.com>
2023-09-14 21:57:22 +0000

    main app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
+import { useState } from "react";
+import Persons from "./Persons";
+import { data } from "./data";
+
 
 function App() {
+  const [persons, setPersons] = useState(data)
+  const clear = () =>{
+    setPersons([]);
+  }
   return (
-    <section>
-      
-    </section>
+    <main>
+      <section>
+        <h3>{persons.length} birthdays today</h3>
+        <Persons people={persons}/>
+        <button type="button" className="btn" onClick={clear}>Clear Me</button>
+      </section>
+    </main>
   );
 }
 
