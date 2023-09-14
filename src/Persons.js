ec2b4d0732360c9138b9da61bb069665f9fe8d63
COBBINA EMMANUEL ADU BOATENG <aduboateng333@gmail.com>
2023-09-14 21:56:36 +0000

    page for each person

--- src/Persons.js
+++ src/Persons.js
@@ -1,8 +1,20 @@
-const Persons = () => {
+import React from "react";
+const Persons = ({people}) => {
     return(
-        <section>
-
-        </section>
+        <>
+        {people.map((persons)=>{
+            const {id, pic, name, age} = persons
+            return(
+                <article key={id}>
+                    <img src={pic} alt="no-pic" />
+                    <div>
+                        <h4>{name}</h4>
+                        <p>{age}</p>
+                    </div>
+                </article>
+            )
+        })}
+        </>
     )
 }
 
