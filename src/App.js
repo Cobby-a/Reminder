38f96c05f294a62095c22a6d4a11e70ae50fe38b
COBBINA EMMANUEL ADU BOATENG <aduboateng333@gmail.com>
2023-09-15 09:32:03 +0000

    simplified the main app

--- src/App.js
+++ src/App.js
@@ -1,20 +1,11 @@
-import { useState } from "react";
 import Persons from "./Persons";
 import { data } from "./data";
 
 
 function App() {
-  const [persons, setPersons] = useState(data)
-  const clear = () =>{
-    setPersons([]);
-  }
   return (
     <main>
-      <section>
-        <h3>{persons.length} birthdays today</h3>
-        <Persons people={persons}/>
-        <button type="button" className="btn" onClick={clear}>Clear Me</button>
-      </section>
+        <Persons people={data}/>
     </main>
   );
 }
