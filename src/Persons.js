f66b5e93daefb1293c0806d0dd88a869d67bf359
COBBINA EMMANUEL ADU BOATENG <aduboateng333@gmail.com>
2023-09-15 09:31:14 +0000

    added a delete icon for each person

--- src/Persons.js
+++ src/Persons.js
@@ -1,20 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
+import DeleteForeverIcon from '@mui/icons-material/DeleteForeverOutlined'
+
 const Persons = ({people}) => {
+    const [persons, setPersons] = useState(people)
+    const removeMe = (id) =>{
+        let remove = persons.filter((person)=> person.id !== id)
+        setPersons(remove)
+    }
+    const clear = () =>{
+        setPersons([]);
+      }
     return(
-        <>
-        {people.map((persons)=>{
-            const {id, pic, name, age} = persons
+        <section>
+        <h3>{persons.length} birthdays today</h3>
+        {persons.map((person)=>{
+            const {id, pic, name, age} = person
             return(
                 <article key={id}>
+                    <div className="person">
                     <img src={pic} alt="no-pic" />
-                    <div>
+                    <div className=".pp">
                         <h4>{name}</h4>
-                        <p>{age}</p>
+                        <p>{age} years</p>
                     </div>
+                    </div>
+                    <DeleteForeverIcon style={{fontSize: "1.8rem", color:"red", cursor:"pointer"}} onClick={()=>removeMe(id)}></DeleteForeverIcon>
                 </article>
             )
         })}
-        </>
+        <button type="button" className="btn" onClick={clear}>Clear Me</button>
+        </section>
     )
 }
 
